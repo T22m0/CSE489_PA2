d0714842939cf16a19a669315eeda4e92df5c602
Issac <issaccho@buffalo.edu>
2016-10-26 15:37:39 -0400

    initial

---
+++ source/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int main(int argc, char* argv[]){
+	if(argc != 5){
+		printf("");
+	}
+	if(strcmp("-i",argv[1])==0 && strcmp("-t",argv[3])==0){
+
+	}else if(strcmp("-i",argv[3])==0 && strcmp("-t",argv[1])==0){
+
+	}else{
+
+	}
+	return 0;
+
+} 
