32df11d76028ddce37f62a29fad61f45afe0a320
Issac <issaccho@buffalo.edu>
2016-11-02 16:54:27 -0400

    update, display, disable works

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ char** tokens;
 //_______________________________//
 int Num_conn;
 int time;
+int packets;
 unsigned int numTok;
 int my_id=0;
 socklen_t sock_len = sizeof(temp.sock_info);
@@ -79,21 +80,22 @@ void printTopology(){
 	sortConnInfo(info.conn_info);
 	//sort topology before printout
 	int i;
-	printf("Number of servers: %d\nNumber of Connction: %d\n",info.num_serv ,info.num_conn);
+	printf("[Number of servers]: %d\n[Number of Connction]: %d\n",info.num_serv ,info.num_conn);
 	for(i =0; i< info.num_serv; i++){
-		printf("SERVER- id:%d, ip:%s, port:%d\n",info.serv_info[i].id,info.serv_info[i].ip,info.serv_info[i].port);
+		printf("[SERVER] id:%d, ip:%s, port:%d\n",info.serv_info[i].id,info.serv_info[i].ip,info.serv_info[i].port);
 	}
 	for(i=0; i< info.num_serv; i++){
-		printf("Connection- src:%d, dst:%d, metric:%d\n",info.conn_info[i].src,info.conn_info[i].dst,info.conn_info[i].metric);
+		printf("[Connection] src:%d, dst:%d, metric:%d\n",info.conn_info[i].src,info.conn_info[i].dst,info.conn_info[i].metric);
 	}
 	//testing
 }
 //print out current topology table
-void syncTable(FILE *top){
+/*void syncTable(FILE *top){
 	int i;
 	rewind(top);
+	//fseek(top,0,SEEK_SET);
 	//heading beginning of the file
-	fprintf(top, "%d\n%d\n",info.num_serv, info.num_conn);
+	fprintf(top,"%d\n%d\n",info.num_serv, info.num_conn);
 	printf("%d is current num_conn\n",info.num_conn);
 	for(i =0; i<info.num_serv;i++){
 		fprintf(top, "%d %s %d\n",info.serv_info[i].id,\
@@ -108,8 +110,17 @@ void syncTable(FILE *top){
 	   } 
 	}
 	fprintf(top,"SUCCESS To doso\n");
-}
+}*/ 
 //output the table to the topology file
+//no need this function...
+int check_id(int i){
+	return (1<=i)&&(i<=info.num_serv);
+}
+//id should be larger than 1, and less than total number of server
+int check_cost(uint16_t i){
+	return (zero<i)&&(i<=inf);
+}
+//cost should be larger than zero, and less than 0xFFFF
 int top_validate(FILE *top){
 	int i;
 	//index int
@@ -189,6 +200,7 @@ int top_validate(FILE *top){
 		my_id == num1 ? (info.conn_info[i].dst = num2) : (info.conn_info[i].dst = num1);
 		//if my_id is same as num1, then dst is num2 or it is num1;
 		if((num1!=num2)&&!(info.conn_info[i].metric = atoi(tokens[2]))){
+		// cost of connection except loopback should be larger than 0!
 			printf("Cost should be larger than 0!!\n");
 			fclose(top);
 			return 0;
@@ -198,7 +210,7 @@ int top_validate(FILE *top){
 	fgets(buf,sizeof(buf),top);
 	if(!feof(top)){
 		printf("Number of connection doesn match with actual connection list\n");
-		printf("This was what was remaining %s\n",buf);
+		printf("This was what was remaining in the file--> %s\n",buf);
 		fclose(top);
 		return 0;
 	}
@@ -227,13 +239,17 @@ int top_validate(FILE *top){
 	}
 	//finding src-dst connection not on the topology file
 	//set cost inf(0xFFFF) to all connection not shown in topology
+	printf("\n_-_-_-_-_-SUCCESS to parse Topology-_-_-_-_-_\n");
+	sleep(2);
 	return 1;
 }
 int init(){
+	packets =0;
+	//initialize packets variable
 	bzero(&svSock,sizeof(svSock));
 	if((svSock.sock = socket(AF_INET,SOCK_DGRAM,0))==-1){
 		perror("ERROR! CANT create socket:");
-		exit(1);
+		return 0;
 	}
 	svSock.sock_info.sin_family = AF_INET;
 	svSock.sock_info.sin_port = htons(info.serv_info[my_id-1].port);
@@ -241,9 +257,11 @@ int init(){
 //	printf("%s is my ip %d my port\n",info.serv_info[my_id-1].ip,info.serv_info[my_id-1].port);
 	if((bind(svSock.sock,(struct sockaddr*)&svSock.sock_info, sock_len))==-1){
 		perror("ERROR! Cant bind:");
-		exit(1);
+		return 0;
 	}	
-	printf("\nSUCCESS to init\n");
+	printf("\n_-_-_-_-_-SUCCESS to Bind socket__-_-_-_-_\n");
+	sleep(2);
+	return 1;
 }
 void runServ(){
 	int fd_count;
@@ -270,10 +288,36 @@ void runServ(){
 			tokens = parseTok(command);
 			if(tokens[0]=='\0') continue;
 			if((strcasecmp(tokens[0],"update")==0)&&numTok==4){
+				int num1 = atoi(tokens[1]);
+				int num2 = atoi(tokens[2]);
+				//parse ids from tokens
+				int metric;
+				strcasecmp(tokens[3],"inf")==0 ? (metric=inf) : strcasecmp(tokens[3],"zero")==0 ? (metric=zero): (metric=atoi(tokens[3]));						//metric could be either 'inf', 'zero' or any numeric value between 0<x<65535
+				if(check_id(num1)&&check_id(num2)&&check_cost(metric)){
+				//check if all arguments are valid
+			   	   if((num1==my_id)||(num2==my_id)){
+				   // check if the user tries to change cost of my connection
+				      if(num1!=num2){
+				      // check if the usier tries to change cost for looopback
+				        int cur_cost;
+					my_id == num1 ? (cur_cost = info.conn_info[num2-1].metric) : (cur_cost = info.conn_info[num1-1].metric);
+					// save cost value for update-to-same-cost case
+					if(cur_cost != metric){
+					// check if cost for update is same as current cost
+					   my_id == num1 ? (info.conn_info[num2-1].metric=metric) : (info.conn_info[num1-1].metric=metric);
+				   	   metric == inf ? info.num_conn-- : info.num_conn++;
+					   // if metric is inf, it is the case same as disable. or it is enabling connection between two hosts
+					   printf("<%s> SUCCESS!\n",tokens[0]);
+					}else printf("<%s> The cost is already set to the value! no need to update\n",tokens[0]);
+				      }else printf("<%s> you cannot update cost for loopback!\n",tokens[0]);
+				   }else printf("<%s> Your arguments does not contain id of current machine!\n",tokens[0]);					
+				}else printf("<%s> Please check arguments for update!\n",tokens[0]);
 			}else if((strcasecmp(tokens[0],"step")==0)&&numTok==1){
 			}else if((strcasecmp(tokens[0],"packets")==0)&&numTok==1){
+				packets = 0;
 			}else if((strcasecmp(tokens[0],"display")==0)&&numTok==1){
 				printTopology();
+				printf("<%s> SUCCESS!\n",tokens[0]);
 			}else if((strcasecmp(tokens[0],"disable")==0)&&numTok==2){
 				int dst = atoi(tokens[1]);
 				//parse given input 
@@ -282,16 +326,15 @@ void runServ(){
 				//	1. dst should be larger than 0
 				//	2. dst should be less than total number of server
 				//	3. dst should not be same as src - cannot update loopback
-				  if(info.conn_info[dst-1].metric != inf){
-				  // if connection is already disabled, skip changing value
+				    if(info.conn_info[dst-1].metric != inf){
+				    // if connection is already disabled, skip changing value
 					info.conn_info[dst-1].metric = inf;
 					info.num_conn--;
-					syncTable(topology);
-					printf("%s SUCCESS\n",tokens[0]);
-				  }
-				  else printf("connection to given server ID is already disabled\n");
+					printf("<%s> SUCCESS\n",tokens[0]);
+				    }
+				    else printf("<%s> connection to given server ID is already disabled\n",tokens[0]);
 				}else{
-				   printf("server ID is not valie\n");
+				   printf("<%s> server ID is not valie\n",tokens[0]);
 				}
 			}else if((strcasecmp(tokens[0],"crash")==0)&&numTok==1){
 			}else printf(USAGE);
@@ -312,41 +355,47 @@ int main(int argc, char* argv[]){
 		printf(PROC_USAGE);
 	//print error if it comes short of number of arguments
 	}else{
-		if(strcmp("-i",argv[3])==0 && strcmp("-t",argv[1])==0){
-		   if(atoi(argv[4])) time = atoi(argv[4]);
-		   else{
-			printf("please give right time interval!\n");
-			exit(1);
-		   }
-		   //test interval validity
-		   topology = fopen(argv[2],"r+");	
-		   int i;
-	           if(topology){
-		      for(i=0; i<2; i++){
-			fgets(buf,sizeof(buf),topology);
-			if(i) Num_conn = atoi(buf);
-		      }
-		   }else printf("File could not be open..\n");
-		   //test file open validity  
-		  if(NUM_SERVER < Num_conn+1){
-			printf("Your number of connection in  topology file is wrong\n");
-			fclose(topology);
-			exit(1);
+  	   if(strcmp("-i",argv[3])==0 && strcmp("-t",argv[1])==0){
+	      if(atoi(argv[4])) time = atoi(argv[4]);
+	      else{
+		 printf("please give right time interval!\n");
+		 exit(1);
+	      }
+	      //test interval validity
+	      topology = fopen(argv[2],"r");	
+	      int i;
+	      if(topology){
+	         for(i=0; i<2; i++){
+	  	    fgets(buf,sizeof(buf),topology);
+		    if(i) Num_conn = atoi(buf);
 		  }
-		  //test max connection
-		  bzero(&pack, sizeof(pack));
-		  bzero(&info, sizeof(info));
-		  FD_ZERO(&readfds);
-		  //init for packet struct 
-		  if(top_validate(topology) && init()){
-		  //test if topology contains correct data
-			printTopology();
-			syncTable(topology);
-			runServ();
-		  }	
-		}else{
-			printf(PROC_USAGE);
-		}
+	      }else printf("File could not be open..\n");
+	      //test file open validity  
+	      if(NUM_SERVER < Num_conn+1){
+		 printf("Your number of connection in  topology file is wrong\n");
+		 fclose(topology);
+		 exit(1);
+	      }
+	      //test max connection
+	      bzero(&pack, sizeof(pack));
+	      bzero(&info, sizeof(info));
+	      FD_ZERO(&readfds);
+	      //init for packet struct 
+	      if(top_validate(topology) && init()){
+	      //test if topology contains correct data
+	 	 if(fclose(topology)!=0){
+			perror("cannot close");
+			exit(1);
+		 }
+		 //no more need the topology
+		 printTopology();
+		 //print beginning result 
+		 runServ();
+		 //run routing.
+	      }else printf("Please check ur topology file\n");	
+	   }else{
+		printf(PROC_USAGE);
+	   }
 	}
 	return 0;
 } 
